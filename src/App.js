57e1596402cfcb5a2c00d4700fbd3d9f16f92830
BirukMetaferia <145568670+BirukMetaferia@users.noreply.github.com>
2024-04-18 01:28:30 +0600

    lower header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from './Components/Header/Header';
+
+import LowerHeader from './Components/Header/LowerHeader';
+
 import './App.css';
 
 function App() {
   return (
-    <div >
-     < Header/>
-    </div>
+    <>
+      <Header/>
+    
+    </>
   );
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,57 +1,67 @@
 import React from "react";
 import classes from './Header.module.css';
-import { FaMapPin } from 'react-icons/fa'; // Import a pin icon from react-icons
-import { BsSearch } from 'react-icons/bs';
-import { BiCart } from 'react-icons/bi';
+import {  FaMapPin } from "react-icons/fa";
+import { BsSearch } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+import LowerHeader from "./LowerHeader";
 
 const Header = () => {
   return (
-    <header className={classes.headerContainer}>
-      <div className={classes.logoContainer}>
-        <a href="/">
+    <>
+    <section>
+    <div className={classes.header__container}>
+
+      <div className={classes.logo__container}>
+        <a href="#">
           <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon Logo" />
         </a>
-      </div>
+      
 
-      <div className={classes.deliveryLocation}>
-        <FaMapPin />
-        <div className={classes.deliveryText}>
-          <span>Deliver to</span>
+      <div className={classes.delivery}>
+        <span>
+          < FaMapPin />
+        </span>
+        <div>
+          <p>Deliver to</p>
           <span>Ethiopia</span>
         </div>
       </div>
-
-      <div className={classes.searchBar}>
-        <select className={classes.searchDropdown} name="categories">
-          <option value="all">All</option>
-          {/* ... other categories ... */}
+      </div>
+      
+      <div className={classes.search}>
+        <select name="" id="">
+          <option value="">All</option>
+          {/* other options not visible */}
         </select>
-        <input type="text" placeholder="Search..." />
-        <BsSearch />
+        <input type="text" />
+        <BsSearch size={25} />
       </div>
-
-      <div className={classes.accountAndOrders}>
-        <a href="/account">
-          <div>
-            <span>Hello, Sign in</span>
-            <span>Account & Lists</span>
-          </div>
+  
+      <div className={classes.order__container}>
+        <a href="#" className={classes.language}>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png" alt="flag of the United States" />
         </a>
-        <a href="/orders">
-          <div>
-            <span>Returns</span>
-            <span>& Orders</span>
-          </div>
+        <select name="" id="">
+          <option value="">EN</option>
+          {/* other options not visible */}
+        </select>
+        <a href="#">
+          <p>Sign In</p>
+          <span>Account & Lists</span>
         </a>
-      </div>
-
-      <div className={classes.cartContainer}>
-        <a href="/cart">
-          <BiCart />
+        <a href="#">
+          <p>Returns</p>
+          <span>& Orders</span>
+        </a>
+        <a href="#" className={classes.cart}>
+          <BiCart size={35} />
           <span>0</span>
         </a>
       </div>
-    </header>
+      </div>
+    </section>
+    <LowerHeader/>
+    </>
   );
 };
 
