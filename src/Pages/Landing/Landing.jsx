15a1a43a164e6d69d5c0783dd837dbb0290e3342
BirukMetaferia <birukm555@gmail.com>
2024-05-17 00:11:56 +0600

    buiild

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,8 +3,11 @@ import Layout from '../../Components/Layout/Layout';
 import Carousel from '../../Components/Carousel/Carousel';
 import Category from '../../Components/Category/Category';
 import Product from '../../Components/Product/Product';
-
+//import Footer from '../../Components/Footer/Footers';
 // Comment Code
+//import Footer from '../../Components/Footer/Footer';
+import Footer from '../../Components/Footer/Footer'; 
+
 
 function Landing() {
   
@@ -15,6 +18,7 @@ function Landing() {
         <Carousel />
         <Category />
         <Product />
+       
       </Layout>
     );
   
