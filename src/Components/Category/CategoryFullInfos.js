d44aed1ed9a5fe4ae6afeff7309d6ca760a99dbb
BirukMetaferia <birukm555@gmail.com>
2024-05-16 22:14:09 +0600

    category new style

--- src/Components/Category/CategoryFullInfos.js
+++ src/Components/Category/CategoryFullInfos.js
@@ -1,23 +1,30 @@
 export const categoryInfos = [
-    {
-      title: "Electronics",
-      name: "electronics",
-      imgLink: "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_2x._SY232_CB667159063_.jpg",
-    },
-    {
-      title: "Discover fashion trends",
-      name: "women's clothing",
-      imgLink: "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Mug_2x._SY232_CB667159063_.jpg",}
+    
+      
+        {
+          title: "Electronics",
+          name: "electronics",
+          imgLink: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg"
+        },
+        {
+          title: "Discover fashion trends",
+          name: "women's clothing",
+          imgLink: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg"
+        },
+        {
+          title: "Men's Clothing",
+          name: "men's clothing",
+          imgLink: "https://m.media-amazon.com/images/I/618bcm65ksL._AC_UL480_FMwebp_QL65_.jpg"
+        }
       ,
-    { title:"Men's clothing",
-    name:"men's clothing",
-        imgLink: "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_2x_v3._SY608_CB573698005_.jpg" ,
-    },
-    {
-        title:"Jewelery",  
-        name:"jewelery",
-        imgLink: "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_2x_v1._SY608_CB573698005_.jpg" ,
-    }
+      
+      {
+        title: "Jewelry",
+        name: "jewelry",
+        imgLink: "https://m.media-amazon.com/images/I/71r7eWuCsaL._AC_UL480_FMwebp_QL65_.jpg"
+      }
+      
+
 
   ];
   
