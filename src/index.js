a9f6ce19adc9bc7b06698048bd2c8e3cc00f52dc
BirukMetaferia <birukm555@gmail.com>
2024-05-17 10:24:04 +0600

    home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot from "react-dom/client"
 import './index.css';
 import App from './App';
 import { DataProvider } from './Components/DataProvider/DataProvider';
 import { initialState, reducer } from './Utility/reducer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root')); // Use createRoot from "react-dom/client"
 
 root.render(
   <React.StrictMode>
