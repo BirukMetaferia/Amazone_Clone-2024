a200fc59e9386af7410ffc39fd6976a1ca9b87c1
BirukMetaferia <birukm555@gmail.com>
2024-05-18 23:43:38 +0600

    footer

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+//import Footer from '../Footer/Footer';
 
 function Layout({ children }) {
   return (
     <div>
       <Header />
       {children}
-      <Footer />
+      
     </div>
   );
 }
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import Layout from '../../Components/Layout/Layout';
 import Carousel from '../../Components/Carousel/Carousel';
 import Category from '../../Components/Category/Category';
 import Product from '../../Components/Product/Product';
-//import Footer from '../../Components/Footer/Footers';
+import Footer from '../../Components/Footer/Footers';
 // Comment Code
 //import Footer from '../../Components/Footer/Footer';
 //
@@ -18,7 +18,7 @@ function Landing() {
         <Carousel />
         <Category />
         <Product />
-       
+        <Footer />
       </Layout>
     );
   
